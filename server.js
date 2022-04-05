057d2d57aa22eb7562c31f11fe6ced2487d2295a
JESSE <dtrsjesse121@gmail.com>
2022-04-05 13:45:23 -0400

    add server, begin making routes

---
+++ server.js
@@ -0,0 +1,92 @@
+//dependencies
+
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const database = require("./db/db.json")
+
+//Express App
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// link assets
+app.use(express.static('public'));
+
+// This sets up data parsing
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//On load get root index
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+// Notes html and it's "url"
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+})
+
+// GET and POST routes
+app.route("/api/notes")
+    .get(function (req, res) {
+        res.json(database);
+    })
+
+    // Add a new note to the json db file.
+    .post(function (req, res) {
+        let jsonFilePath = path.join(__dirname, "/db/db.json");
+        let newNote = req.body;
+        let highestId = 99;
+        for (let i = 0; i < database.length; i++) {
+            let individualNote = database[i];
+
+            if (individualNote.id > highestId) {
+                highestId = individualNote.id;
+            }
+        }
+        newNote.id = highestId + 1;
+        //push to db.json.
+        database.push(newNote)
+
+        // Write the db.json file again.
+        fs.writeFile(jsonFilePath, JSON.stringify(database), function (err) {
+
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Your note was saved!");
+        });
+        // Gives back the response, which is the user's new note. 
+        res.json(newNote);
+    });
+
+//DELETE 
+app.delete("/api/notes/:id", function (req, res) {
+    let jsonFilePath = path.join(__dirname, "/db/db.json");
+    // request to delete note by id.
+    for (let i = 0; i < database.length; i++) {
+
+        if (database[i].id == req.params.id) {
+            // Splice takes i position, and then deletes the 1 note.
+            database.splice(i, 1);
+            break;
+        }
+    }
+    // Write the db.json file again.
+    fs.writeFileSync(jsonFilePath, JSON.stringify(database), function (err) {
+
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log("Your note was deleted!");
+        }
+    });
+    res.json(database);
+});
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
